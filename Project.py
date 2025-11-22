357eced18c26f345cf5aaebb14c735b3394675ad
Riion8 <soer8850@gmail.com>
2025-11-22 13:56:39 -0600

    v2

--- Project.py
+++ Project.py
@@ -8,7 +8,7 @@ import happybase
 spark = SparkSession.builder.appName("Soer Final Project").enableHiveSupport().getOrCreate()
 
 # Step 2: Load the data from the Hive table 'diamonds' into a Spark DataFrame
-diamonds_df = spark.sql("SELECT * FROM diamonds")
+diamonds_df = spark.sql("SELECT carat, cut, color, clarity, depth, table, x, y, z, price  FROM diamonds")
 
 # Step 3: Handle null values by either dropping or filling them
 diamonds_df = diamonds_df.na.drop()  # Drop rows with null values
